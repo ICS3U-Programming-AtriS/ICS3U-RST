d1728c681fe8a4ac6fc75bb35473e5512464f136
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-05-30 15:52:30 +0000

    RST-02 COMPLETE

--- code.py
+++ code.py
@@ -3,10 +3,32 @@
 # Date: May 28, 2025
 # RST
 
+import ugame 
+import stage
+
 
 def main():
-    # Display message
-    print("I yearn for flesh, Atri.")
+    # Image bank for the background
+    image_bank_background = stage.Bank.from_bmp16("bank1.bmp")
+
+    # Set the background to the first image in the image bank
+    background = stage.Grid(image_bank_background, 20, 8)
+
+    # display the background at 60fps
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
+
+    # Hacked up a scrolling background
+    scroll = -50
+    while True:
+        background.move(scroll,0)
+        scroll += 1
+        if scroll == 0:
+            scroll = -32
+        game.tick()
+        game.render_block()
 
 
-main()
+if __name__ == "__main__":
+    main()
